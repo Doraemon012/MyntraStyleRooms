008ba4be5662c3c339ccb1fd81a2e9df8c3111cc
Jasmine <jasminej5220@gmail.com>
2025-10-08 00:46:16 +0530

    Metro: add aliases for semver/functions/* to satisfy react-native-reanimated worklets validator

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,9 @@ config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 // WebRTC module resolution
 config.resolver.alias = {
   'react-native-webrtc': 'react-native-webrtc',
+  // Ensure reanimated's validate-worklets can resolve deep semver paths
+  'semver/functions/satisfies': require.resolve('semver/functions/satisfies.js'),
+  'semver/functions/prerelease': require.resolve('semver/functions/prerelease.js'),
 };
 
 // Disable source maps in development to avoid InternalBytecode.js issues
